0390aefe0ccd4f56ef2bd21d793373a48525326b
Lynn Bradshaw <readyready15728@gmail.com>
2020-09-09 10:03:14 -0400

    Updating solution #6

--- solution-006.rb
+++ solution-006.rb
@@ -3,6 +3,6 @@
 gets
 
 puts (readlines.map do |line|
-  pair_of_integers = line.split.map { |x| x.to_f }
-  (pair_of_integers[0] / pair_of_integers[1]).round
+  a, b = line.split.map { |x| x.to_f }
+  (a / b).round
 end).join ' '
