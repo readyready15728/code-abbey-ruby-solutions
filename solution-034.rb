50a3f66df7cc185d2003ff4d424d5bab4bfa32df
Lynn Bradshaw <readyready15728@gmail.com>
2020-12-26 11:03:28 -0500

    Adding solution #34 and test data

---
+++ solution-034.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+gets
+parameter_list = readlines.map { |line| line.split(' ').map &:to_f }
+
+def expression(a, b, c, d, x)
+  a * x + b * x**1.5 - c * Math.exp(-x / 50) - d
+end
+
+puts (parameter_list.map do |parameters|
+  a, b, c, d = parameters
+  low, high = 0.to_f, 100.to_f
+  epsilon = 1e-7
+
+  while true
+    mid = (low + high) / 2
+
+    if expression(a, b, c, d, mid) < 0
+      low = mid
+    elsif expression(a, b, c, d, mid) > 0
+      high = mid
+    end
+
+    break if -epsilon < expression(a, b, c, d, low) and expression(a, b, c, d, low) < epsilon
+  end
+
+  low
+end).join ' '
